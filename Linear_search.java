1f71eb2977eb007b7af35c2d9145ad30dd525226
INDUSAKTHIVEL <indusakthi2003@gmail.com>
2023-07-19 19:34:09 +0530

    linear

---
+++ Linear_search.java
@@ -0,0 +1,32 @@
+package Linear;
+
+import java.util.Scanner;
+
+public class Linear_search {
+
+	public static void main(String[] args) {
+				    Scanner sc=new Scanner(System.in);
+				    int n=sc.nextInt();
+			       
+			        int B = sc.nextInt();
+			        int[] A = new int[n];
+			        for(int i=0;i<n;i++) {
+			        
+			        	A[i]=sc.nextInt();
+			        	
+			        }
+			        int occurrences = countOccurrences(A, B);
+			        
+			        System.out.println("Number of occurrences: " + occurrences);
+			    }
+			    
+			    public static int countOccurrences(int[] arr, int target) {
+			        int count = 0;
+			        for (int num : arr) {
+			            if (num == target) {
+			                count++;
+			            }
+			        }
+			        return count;
+			    }
+			}
